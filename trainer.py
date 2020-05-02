e2696c4dbbece868f861c500ac18aaca7c50e31a
RMUA <ruzicka_marcel@yahoo.com>
2020-05-02 16:46:18 +0200

    Is_ready disabled
    The function is_ready in trainer.py was erased.

--- trainer.py
+++ trainer.py
@@ -30,7 +30,7 @@ class Trainer:
       #try: use with the except conditions below the block in Python2
       user_input = input("Type in a warrior: ")
       for warrior in self.characters:
-        if warrior.name == user_input:
+        if warrior.name == user_input and warrior.current_health > 0:
           self.active = warrior
           print("{} is now active for trainer {}\n".format(self.active.name.upper(), self.name.upper()))
           break
@@ -38,23 +38,16 @@ class Trainer:
         print("You must activate a warrior from your list. Try again! \n")
       #except NameError:
         #print("Use quotation marks \"\" for inputs")
-        
-  def is_ready(self):
-    if self.active.current_health > 0:
-      return True
-    else:
-      self.active = None
-      return False
           
   def attack_trainer(self, opponent):
-    if self.is_ready() == True:
+    if self.active.current_health > 0:
       if self.name == opponent.name:
         print("Trainer {} can not battle himself!".format(self.name))
         return
       elif self.name != opponent.name:
         self.active.attack(opponent.active)
-        print("Trainer {} brought it on trainer {}.\n".format(self.name, opponent.name))
+        print("Trainer {} has attacked trainer {}.\n".format(self.name, opponent.name))
         return
     else:
-      print("Your warrior is KO. Activate another warrior now!")
+      print("Your warrior is KO, {}. Activate another warrior now!".format(self.name))
       return self.activate_warrior()
